3c524b0625d110af2cefe92f0dcf2328f6f39f3e
setareA <setareaskari79@gmail.com>
2019-01-10 00:48:56 +0330

    heap module added

---
+++ heap.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+
+class HeapNode:
+        def __init__(self, char, freq):
+            self.char = char
+            self.freq = freq
+            self.left = None
+            self.right = None
+
+        def __lt__(self, other):
+            return self.freq < other.freq
+
+        def __eq__(self, other):
+            if other is None:
+                return False
+            if not isinstance(other, HeapNode):
+                return False
+            return self.freq == other.freq
+
+        def get_freq(self):
+            return self.freq
+
+        def get_char(self):
+            return self.char
+
+        def get_right(self):
+            return self.right
+
+        def get_left(self):
+            return self.left
+
+        def set_right(self, r):
+            self.right = r
+
+        def set_left(self, l):
+            self.left = l
